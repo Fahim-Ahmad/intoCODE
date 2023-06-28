66b9a65515aa7d18ed60567800e39c212ff5ecca
atrcodebase <39668413+Fahim-Ahmad@users.noreply.github.com>
2023-06-28 21:26:09 +0430

    split footer into two parts

--- Content Management System/final_project/grey_stairwell/footer.php
+++ Content Management System/final_project/grey_stairwell/footer.php
@@ -1,6 +1,12 @@
   <div id="footer">  
   
-    &copy; <?php echo date("Y"); ?> | <?php wp_nav_menu( array('menu' => 'Footer Navigation Menu', 'theme_location' => 'Footer Navigation Menu' )); ?>
+    <div id="footer-right">
+    <?php wp_nav_menu( array('menu' => 'Footer Navigation Menu', 'theme_location' => 'Footer Navigation Menu' )); ?>
+    </div>
+    
+    <div id="footer-left">
+      &copy; <?php echo date("Y"); ?> All rights reserved.
+    </div>
   
   </div><!--close footer-->
 
