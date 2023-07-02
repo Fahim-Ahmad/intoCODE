a342dc3d2833353a30864620fcd7078dc838e875
atrcodebase <39668413+Fahim-Ahmad@users.noreply.github.com>
2023-07-02 16:38:16 +0430

    Update searchform.php

--- Content Management System/final_project/grey_stairwell/searchform.php
+++ Content Management System/final_project/grey_stairwell/searchform.php
@@ -1,6 +1,6 @@
 <form action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" method="get">
     <div>
-        <input type="text" value="" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="Suchen" />
+        <input type="text" value="" name="s" id="s">
+        <input type="submit" id="searchsubmit" value="Suchen">
     </div>
 </form>
