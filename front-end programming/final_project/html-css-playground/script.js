ed3458e57ad1163953e91ef09a1b7aac2d37c39c
atrcodebase <39668413+Fahim-Ahmad@users.noreply.github.com>
2023-07-03 20:50:46 +0430

    Initial commit

---
+++ front-end programming/final_project/html-css-playground/script.js
@@ -0,0 +1,78 @@
+function displayResult() {
+  var htmlElement = document.getElementById("html").value;
+  var cssElement = document.getElementById("css").value;
+
+  var result = document.querySelector(".output-container");
+  result.innerHTML = '<div class="html-output">' + htmlElement + "</div>";
+
+  var cssStyle = document.createElement("style");
+  cssStyle.textContent = ".html-output " + cssElement;
+  result.prepend(cssStyle);
+}
+
+function handleDragOver(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = "copy";
+  // console.log(event.dataTransfer.dropEffect);
+}
+
+function handleFileDrop(event) {
+  event.preventDefault();
+
+  var files = event.dataTransfer.files;
+  // console.log('files:', files);
+
+  var filesTable = document.querySelector(".files-list");
+  // console.log('filesTable:', filesTable);
+
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    // console.log('file:', file);
+
+    var fileName = document.createElement("span");
+    fileName.innerText = file["name"];
+    // console.log('fileName:', fileName);
+
+    var uuid = document.createElement("span");
+    uuid.className = "hidden";
+    uuid.innerText = URL.createObjectURL(file);
+    // console.log('uuid:', uuid);
+
+    var copyBtn = document.createElement("button");
+    copyBtn.innerText = "Copy";
+    copyBtn.classList.add("copy-btn");
+    copyBtn.addEventListener("click", createCopyHandler(uuid.innerText));
+
+    var showBtn = document.createElement("button");
+    showBtn.innerText = "Preview";
+    showBtn.classList.add("show-btn");
+    showBtn.addEventListener("click", createShowHandler(uuid.innerText));
+
+    var tableRow = document.createElement("tr");
+    tableRow.appendChild(document.createElement("td")).appendChild(fileName);
+    tableRow.appendChild(document.createElement("td")).appendChild(uuid);
+    tableRow.appendChild(document.createElement("td")).appendChild(copyBtn);
+    tableRow.appendChild(document.createElement("td")).appendChild(showBtn);
+
+    filesTable.appendChild(tableRow);
+  }
+
+  console.log(filesTable);
+}
+
+function createCopyHandler(text) {
+  return function () {
+    var dummyElement = document.createElement("textarea");
+    dummyElement.value = text;
+    document.body.appendChild(dummyElement);
+    dummyElement.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummyElement);
+  };
+}
+
+function createShowHandler(url) {
+  return function () {
+    window.open(url, "_blank");
+  };
+}
