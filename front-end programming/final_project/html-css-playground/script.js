5f17b612fabb3ba9983c5646c2c45bab6971f82f
atrcodebase <39668413+Fahim-Ahmad@users.noreply.github.com>
2023-07-09 00:06:55 +0430

    add the '.output-box' text before each css element - method 02

--- front-end programming/final_project/html-css-playground/script.js
+++ front-end programming/final_project/html-css-playground/script.js
@@ -8,25 +8,51 @@ function displayResult() {
   resultElement.innerHTML = '<div class="output-box">' + htmlElement + "</div>";
   // console.log(resultElement);
 
-  var cssElement = cssElement.split("}");
-  // console.log(cssElement)
-  var cssElementUpdate = "";
-  for (var i = 0; i < cssElement.length; i++) {
-    var line = cssElement[i].trim();
-    // console.log(line)
-    // console.log(line.length);
-    // console.log(line.length > 0);
-    if (line.length > 0) {
-    cssElementUpdate += '.output-box ' + line + '}\n';
+  // add '.output-box' text before each css selector - method 01
+  // var cssElement = cssElement.split("}");
+  // // console.log(cssElement)
+  // var cssElementUpdate = "";
+  // for (var i = 0; i < cssElement.length; i++) {
+  //   var line = cssElement[i].trim();
+  //   // console.log(line)
+  //   // console.log(line.length);
+  //   // console.log(line.length > 0);
+  //   if (line.length > 0) {
+  //   cssElementUpdate += '.output-box ' + line + '}\n';
+  //   }
+  // }
+  // var cssStyle = document.createElement("style");
+  // // console.log(cssStyle);
+  // cssStyle.textContent = cssElementUpdate;
+  // console.log(cssStyle);
+
+  // add '.output-box' text before each css selector - method 02
+  var modifiedCSS = cssElement.trim();
+  var modifiedCSS = modifiedCSS.replace(/\n/gs, "");
+  var modifiedCSS = modifiedCSS.replace(/}/g, "}\n\n");
+  var modifiedCSS = modifiedCSS.replace(/{/g, "{\n\n");
+  var modifiedCSS = modifiedCSS.replace(/\*\//g, "*/\n\n");
+
+  var updatedCSS = "";
+  splitCSS = modifiedCSS.split("\n");
+  for (var i = 0; i < splitCSS.length; i++) {
+    var line = splitCSS[i].trim();
+    if (line.includes("{") && !line.startsWith("@") && !/^[0-9]/.test(line)) {
+        // console.log('.output-box', line);
+        updatedCSS += '.output-box ' + line ;
+    } else {
+        // console.log(line);
+        updatedCSS += line + '\n';
     }
   }
 
   var cssStyle = document.createElement("style");
   // console.log(cssStyle);
-  cssStyle.textContent = cssElementUpdate;
-  console.log(cssStyle);
+  cssStyle.textContent = updatedCSS;
+  // console.log(cssStyle);
 
   resultElement.prepend(cssStyle);
+  console.log(resultElement);
 }
 
 function handleDragOver(event) {
